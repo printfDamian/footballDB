b534d9661ac5ef9f250dac58a85f4585bfe286ca
SamuelSantos <samueldamiaosantos@gmail.com>
2024-03-20 16:32:53 +0000

    Leagues, Teams and Players showing up in Bootsrap tables

--- server.js
+++ server.js
@@ -22,41 +22,47 @@ app.get('/data', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    dbConnection.query('SELECT * FROM leagues', (err, results) => {
-      if (err) {
-        console.error(err.message);
-        return res.status(500).send(err);
-      }
-      res.render('index', { leagues: results });
-    });
+  dbConnection.query('SELECT * FROM leagues', (err, results) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send(err);
+    }
+
+    // Add this block
+    
+
+    res.render('index', { leagues: results });
+  });
 });
 
+// Fetch teams by league
 app.get('/teams', function(req, res) {
   var leagueId = req.query.id; // Get the league ID from the query parameter
 
   // Execute your SQL query
-  dbConnection.query('SELECT * FROM teams WHERE league_id = ?', [leagueId], function(error, results) {
+  dbConnection.query('SELECT teams.*, leagues.name AS league_name FROM teams JOIN leagues ON teams.league_id = leagues.id WHERE teams.league_id = ?', [leagueId], function(error, results) {
       if (error) {
           console.error(error.message);
           return res.status(500).send(error);
       } else {
-          // Render the teams page and pass the teams info to the template
-          res.render('teams', { teams: results, leagueId: leagueId });
+          // Render the teams page and pass the teams info and the league name to the template
+          res.render('teams', { teams: results, leagueName: results[0].league_name });
       }
   });
 });
 
+// Fetch players by team
 app.get('/players', function(req, res) {
   var teamId = req.query.id; // Get the team ID from the query parameter
 
   // Execute your SQL query
-  dbConnection.query('SELECT * FROM players WHERE club_team_id = ?', [teamId], function(error, results) {
+  dbConnection.query('SELECT players.*, teams.name AS team_name FROM players JOIN teams ON players.club_team_id = teams.id WHERE players.club_team_id = ?', [teamId], function(error, results) {
       if (error) {
           console.error(error.message);
           return res.status(500).send(error);
       } else {
-          // Render the players page and pass the players info to the template
-          res.render('players', { players: results, teamId: teamId });
+          // Render the players page and pass the players info and the team name to the template
+          res.render('players', { players: results, teamName: results[0].team_name });
       }
   });
 });
